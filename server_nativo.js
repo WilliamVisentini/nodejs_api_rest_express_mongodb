466db8e354a6624b8aeb5f445e51e7fba9f7724a
William <williamvisentini@gmail.com>
2024-11-26 21:59:14 -0300

    Aula-01

--- server_nativo.js
+++ server_nativo.js
@@ -9,10 +9,12 @@ const routes = {
 
 const server = http.createServer((req, res) => {
     res.writeHead(200, { "Content-Type": "text/plain" });
-    res.end(routes[req.url]);
+    //res.end(routes[req.url]);
+    res.end('Curso de Node.js');
 })
 
 server.listen(PORT, () => {
+    //console.log('servidor escutando!');
     console.log(`Server is running at http://localhost:${PORT}`);
 });
 
